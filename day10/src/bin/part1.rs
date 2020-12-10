02cf9461f943c2345e8944e4d53c772e63f78003
irevoire <irevoire@protonmail.ch>
2020-12-10 17:06:00 +0100

    add day10

---
+++ day10/src/bin/part1.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let mut input = aoc::parser::lines_from_args_as::<usize>(1).collect::<Vec<usize>>();
+
+    input.sort();
+
+    let input = input
+        .windows(2)
+        .map(|el| el[1] - el[0])
+        .collect::<Vec<usize>>();
+
+    let (one, three) = (
+        input.iter().filter(|&el| *el == 1).count() + 1,
+        input.iter().filter(|&el| *el == 3).count() + 1,
+    );
+
+    println!("res: {} * {} = {}", one, three, one * three);
+}
---
+++ day10/src/bin/part2.rs
@@ -0,0 +1,29 @@
+#![recursion_limit = "2000"]
+
+fn main() {
+    let mut input = aoc::parser::lines_from_args_as::<usize>(1).collect::<Vec<usize>>();
+
+    input.push(0);
+    input.sort();
+    input.push(input.last().unwrap() + 3);
+
+    let input = &mut input.windows(2).map(|el| el[1] - el[0]);
+
+    let res: usize = std::iter::repeat_with(|| {
+        input
+            .skip_while(|el| *el == 3)
+            .take_while(|el| *el == 1)
+            .count()
+    })
+    .take_while(|el| *el != 0)
+    .map(|el| match el {
+        0 | 1 => 1,
+        2 => 2,
+        3 => 4,
+        4 => 7,
+        _ => unimplemented!(),
+    })
+    .product();
+
+    println!("res: {:?}", res);
+}
